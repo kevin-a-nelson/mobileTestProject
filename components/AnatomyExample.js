1a62429860082a957510780e50741cd668670a9d
SamuraiViking <nelson67@stolaf.edu>
2020-05-03 22:10:56 -0500

    add footer

--- components/AnatomyExample.js
+++ components/AnatomyExample.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Item, Input, Text, List } from 'native-base';
+import { Container, Header, Content, Item, Input, Text, List, Button, View, Grid, Col, Row, Footer, FooterTab, Icon } from 'native-base';
 import Person from './Person'
+import People from './Person';
 
 export default class AnatomyExample extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            people: [1, 2, 3, 4],
+            people: [],
+            allPeople: [],
+            search: "",
+            tempSearch: "",
         };
     }
 
@@ -16,9 +20,12 @@ export default class AnatomyExample extends Component {
         })
             .then((response) => response.json())
             .then((responseJson) => {
-                responseJson = responseJson.results.slice(1, 10)
+                responseJson = responseJson.results
+                responseJson = responseJson.filter(person => person.classYear)
+
                 this.setState({
-                    people: responseJson
+                    people: responseJson,
+                    allPeople: responseJson
                 })
             })
             .catch((error) => {
@@ -26,24 +33,68 @@ export default class AnatomyExample extends Component {
             });
     }
 
-    render() {
+    people() {
 
-        const people = this.state.people.map(person => (
-            <Person person={person} key={person.email}>
-            </Person>
+        if (this.state.people == []) {
+            return []
+        }
+
+        if (this.state.search == "") {
+            return []
+        }
+
+        filteredPeople = this.state.people.filter(person => person.displayName.includes(this.state.search))
+
+        return filteredPeople.map(person => (
+            <Person person={person} key={person.email}></Person>
         ))
+    }
+
+    render() {
 
         return (
             <Container>
-                <Item regular>
-                    <Input placeholder="Search" />
-                </Item>
+                <View style={{ padding: 20 }}>
+                    <Item regular>
+                        <Input placeholder="Find Someone By Name"
+                            onChangeText={tempSearch => this.setState({ tempSearch })}
+                        />
+                    </Item>
+                    <View style={{ marginTop: 20 }}>
+                        <Button
+                            style={{ width: 85 }}
+                            primary
+                            onPress={() => this.setState({ search: this.state.tempSearch })}>
+                            <Text>Enter</Text>
+                        </Button>
+                    </View>
+                </View>
                 <Content>
                     <List>
-                        {people}
+                        {this.people()}
                     </List>
                 </Content>
-            </Container>
+                <Footer>
+                    <FooterTab>
+                        <Button vertical>
+                            <Icon name="apps" />
+                            <Text>Find Someone</Text>
+                        </Button>
+                        <Button vertical>
+                            <Icon name="apps" />
+                            <Text>My Lists</Text>
+                        </Button>
+                        {/* <Button vertical active={true}>
+                            <Icon name="navigate" />
+                            <Text>Navigate</Text>
+                        </Button>
+                        <Button vertical>
+                            <Icon name="person" />
+                            <Text>Contact</Text>
+                        </Button> */}
+                    </FooterTab>
+                </Footer>
+            </Container >
         );
     }
 }
